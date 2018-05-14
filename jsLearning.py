48637294962ab64743c6326152e69475b0f3f31b
LeShine <358368802@qq.com>
2018-05-15 00:30:07 +0800

    js str$arr

--- jsLearning.py
+++ jsLearning.py
@@ -7,7 +7,7 @@ JS笔记
         ==: 自动转换数据类型再比较
         ===：先比较数据类型再比较值
 
-    字符串：‘单引号’、”双引号“、`反引号代表多行字符串`
+    字符串：'单引号'、"双引号"、`反引号代表多行字符串`
         
         字符串方法：
             获取长度：s.length
@@ -24,6 +24,27 @@ JS笔记
     null undefined:
 
     数组：可包含任意数据类型
+        
+        数组方法：
+            获取长度：arr.length
+            *改变arr的length会导致arr大小变化，对arr索引赋值会修改arr，如果赋值                 索引超过arr length，arr的大小会变化
+            搜索指定元素位置：arr.indexOf('x')
+            截取arr部分元素：arr.slice(m,n)  m <= arr < n
+                             arr.slice(m) m <= arr
+                             arr.slice() arr
+            向arr末尾添加若干元素：arr.push(x)
+            删除arr末尾元素：arr.pop()
+            向arr头部添加若干元素：arr.unshift(x)
+            删除arr头部元素：arr.shift()
+            arr排序：arr.sort()
+            arr反转：arr.reverse()
+            修改arr的万能方法：splice()
+                arr.splice(m,n,a,b):从m索引开始删除n个元素，添加a,b
+                arr.splice(m,n)：从m索引开始删除n个元素，不添加
+                arr.splice(2,0)：不删除
+            连接两个arr：arr.concat(arr) *返回新的arr
+            用指定字符连接arr所有元素：arr.join('char') *返回字符串
+
 
     对象：键值对的组合
 
@@ -33,3 +54,5 @@ JS笔记
 严格模式：严格模式下所有变量必须先声明再使用，不声明直接使用的变量是全局变量
     
     strict：”use strict“放在js代码第一行
+
+
